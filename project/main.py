229666d8f8e7c19bfa2a7d8081a86338397eff69
lsm1987 <1987lsm@gmail.com>
2020-03-25 22:29:17 +0900

    [Config] 1. 실행 시 config 파일로부터 설정 읽어오도록 수정

--- project/main.py
+++ project/main.py
@@ -1,6 +1,21 @@
+import sys
+import json
 from data_exporter import data_export
 
-source_data_dir_path = 'docs/sample/data_tables'
-asset_data_dir_path = 'docs/sample/data_assets'
+# argv[1]: 설정 파일 경로
+def main():
+    config_file_path = sys.argv[1] if len(sys.argv) >= 2 else ''
+    if not config_file_path:
+        raise Exception('No config file path')
 
-data_export(source_data_dir_path, asset_data_dir_path)
+    with open(config_file_path) as config_file:
+        config_data = json.load(config_file)
+        #print(config_data)
+
+        src_path = config_data["table_dir_path"]
+        dest_path = config_data["asset_dir_path"]
+
+        data_export(src_path, dest_path)
+
+if __name__ == '__main__':
+    main()
